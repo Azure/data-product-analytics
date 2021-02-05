c2fa7fcdc2b316c355569f45bd78f6a98638879c
Marvin Buss <marvin.buss@gmail.com>
2021-02-05 15:43:04 +0100

    fixed potential bug in parameter replacement

--- configs/UpdateParameters.ps1
+++ configs/UpdateParameters.ps1
@@ -89,7 +89,7 @@ foreach ($config in $configs) {
 
         # Set Content of Parameter File
         Write-Host "Setting Content of Parameter File"
-        $parameterFile | ConvertTo-Json | Set-Content -Path $config.filePath
+        $parameterFile | ConvertTo-Json -Depth 100 | Set-Content -Path $config.filePath
     }
     elseif (($config.fileType.ToLower() -eq "yaml") -or ($config.fileType.ToLower() -eq "yml")) {
         # Load YAML Deployment File
